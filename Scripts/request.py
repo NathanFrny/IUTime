955f3a298f7aa6a989569737dbce7824d4aadf9e
FOURNY Nathan <nathanfourny.pro@gmail.com>
2023-05-25 18:27:57 +0200

    add function lessons_TP
    lessons_TP : the function returns all classes for the day for TPA students

--- Scripts/request.py
+++ Scripts/request.py
@@ -7,42 +7,61 @@ from datetime import timedelta
 ics_file = './Calendar/ADECal.ics'
 
 # Lecture du fichier iCalendar
-with open(ics_file, 'r') as file:
+with open(ics_file, 'r', encoding="utf-8") as file:
     ical_data = file.read()
 
 # Analyse du fichier iCalendar
 calendar = Calendar(ical_data)
 
-# Variables pour requêtes
-myTP = "TPA"
-myTD = "TD1"
-TPA_mod_size = 0
-TPA_only_mod_size = 0
-TD1_mod_size = 0
+# Récupère la date du jour
+reference_date = datetime.date.today()
 
-# Parcourir les événements du calendrier
-for event in calendar.events:
-    # Convertir la date de début en UTC + 2h pour se mettre à l'heure
-    start_utc = event.begin.astimezone(timezone('UTC')) + timedelta(minutes=120)
 
-    # Convertir la date de fin en UTC + 2h pour se mettre à l'heure
-    end_utc = event.end.astimezone(timezone('UTC')) + timedelta(minutes=120)
 
-    # Récupère le nom de l'évênement actuel
-    module_name = event.name
+
+
+# Renvoies les cours de la journée pour le TP mis en paramètre
+def lessons_TP(tp: str):
     
-    # Vérifie le nombre de cours restants des étudiants du TPA
-    if module_name[-3:] == myTP or module_name[-3:] == myTD or module_name[-3:] == " CM" or module_name[-3:] == " DS" or module_name[-3:] == "A 1" or module_name[-3:] == "A 2" or module_name[-3:] == " Ds" or module_name[-3:] == "PFT" or module_name[-3:] == "P A":
-        TPA_mod_size += 1
-    # Vérifie le nombre de cours restants des étudiants du TPA en TP
-    if module_name[-3:] == myTP:
-        TPA_only_mod_size += 1
-    # Vérifie le nombre de cours restants des étudiants du TPA en TD
-    if module_name[-3:] == myTD:
-        TD1_mod_size += 1
+    lessons = {}
+    
+    for event in calendar.events:
         
-print()
-print("Nombre de cours restant pour le TPA :    ", TPA_mod_size)
-print("Nombre de cours restant du TPA en TP :   ", TPA_only_mod_size)
-print("Nombre de cours restant du TPA en TD :   ", TD1_mod_size)
-print()
+        # Convertir la date de début en UTC + 2h pour se mettre à l'heure
+        start_utc = event.begin.astimezone(timezone('UTC')) + timedelta(minutes=120)
+        # Convertir la date de fin en UTC + 2h pour se mettre à l'heure
+        end_utc = event.end.astimezone(timezone('UTC')) + timedelta(minutes=120)
+        
+        # Si l'évênement est aujourd'hui
+        if start_utc.date() == reference_date:
+            
+            if event.name[-3:] == "TPA" or event.name[-3:] == "TD1" or event.name[-3:] == " CM" or event.name[-3:] == " DS" or event.name[-3:] == "A 1" or event.name[-3:] == "A 2" or event.name[-3:] == " Ds" or event.name[-3:] == "PFT" or event.name[-3:] == "P A":
+                
+                start_hour = start_utc.strftime("%H:%M")
+                end_hour = end_utc.strftime("%H:%M")
+                
+                # On récupère les profs du cours
+                description = event.description
+                lines = description.split('\n')
+                # Suppression des lines contenant "BUT" ou des parenthèses
+                teacher= [ligne for ligne in lines if "BUT" not in ligne and "(" not in ligne and ")" not in ligne and ligne.strip() != ""]
+                
+                
+                # Si il y a un profs alors on récupère uniquement le teacheren string sinon on indique qu'il n'y a aucun prof
+                if len(teacher) > 0:
+                    teacher= teacher[0]
+                else:
+                    teacher= "Aucun prof"
+                
+                # On crée la valeur du dictionnaire contenant le cours (la clé est l'heure de début du cours)
+                lessons[end_hour] = {"Cours" : event.name[:-3],
+                                "Salle" : event.location,
+                                "Prof" : teacher,
+                                "Heure de fin" : end_hour}
+    
+    return lessons
+
+
+
+
+print(lessons_TP("TPA"))
--- Scripts/tests/test3.py
+++ Scripts/tests/test3.py
@@ -35,3 +35,38 @@ for event in calendar.events:
     # Comparer la date actuel avec la date de début du module pour filtrer les événements au jour même
     #if start_utc.date() == reference_date:
 
+
+
+"""# Variables pour requêtes
+myTP = "TPA"
+myTD = "TD1"
+TPA_mod_size = 0
+TPA_only_mod_size = 0
+TD1_mod_size = 0
+
+# Parcourir les événements du calendrier
+for event in calendar.events:
+    # Convertir la date de début en UTC + 2h pour se mettre à l'heure
+    start_utc = event.begin.astimezone(timezone('UTC')) + timedelta(minutes=120)
+
+    # Convertir la date de fin en UTC + 2h pour se mettre à l'heure
+    end_utc = event.end.astimezone(timezone('UTC')) + timedelta(minutes=120)
+
+    # Récupère le nom de l'évênement actuel
+    module_name = event.name
+    
+    # Vérifie le nombre de cours restants des étudiants du TPA
+    if module_name[-3:] == myTP or module_name[-3:] == myTD or module_name[-3:] == " CM" or module_name[-3:] == " DS" or module_name[-3:] == "A 1" or module_name[-3:] == "A 2" or module_name[-3:] == " Ds" or module_name[-3:] == "PFT" or module_name[-3:] == "P A":
+        TPA_mod_size += 1
+    # Vérifie le nombre de cours restants des étudiants du TPA en TP
+    if module_name[-3:] == myTP:
+        TPA_only_mod_size += 1
+    # Vérifie le nombre de cours restants des étudiants du TPA en TD
+    if module_name[-3:] == myTD:
+        TD1_mod_size += 1
+        
+print()
+print("Nombre de cours restant pour le TPA :    ", TPA_mod_size)
+print("Nombre de cours restant du TPA en TP :   ", TPA_only_mod_size)
+print("Nombre de cours restant du TPA en TD :   ", TD1_mod_size)
+print()"""
